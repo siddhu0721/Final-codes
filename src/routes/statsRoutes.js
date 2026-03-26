aefbc430f56b9733a52fdf29aa11ef5ee1ad36de
Shreyansh Dewangan <shreyanshdewangan727@gmail.com>
2026-03-26 21:01:43 +0530

    Delete src directory

--- src/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const statsController = require("../controllers/statsController");
-const auth = require("../middleware/authMiddleware");
-
-router.get("/", auth.protect, statsController.getManagerStats);
-
-module.exports = router;
